49a779de80cafd5fdcd9c60ee6da3b9bb3461daa
raysc <raysc@RYA>
2024-03-15 23:10:43 -0400

    Add project files.

---
+++ Dictionary(E)/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Dictionary<string, string> players = new Dictionary<string, string>
+        {
+            {"P1", "Chris"},
+            {"P2", "James"},
+            {"P3", "Mark"},
+            {"P4", "Ethan"},
+            {"P5", "Brad"}
+        };
+
+       
+        Console.WriteLine("Keys:");
+        foreach (var key in players.Keys)
+        {
+            Console.WriteLine(key);
+        }
+
+        
+        Console.WriteLine("\nValues:");
+        foreach (var value in players.Values)
+        {
+            Console.WriteLine(value);
+        }
+
+       
+        Console.WriteLine("\nKeys and Values:");
+        foreach (var kvp in players)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+
+        
+        if (players.ContainsKey("P3"))
+        {
+            players.Remove("P3");
+            Console.WriteLine("\nplayer with key 'P3' removed.");
+        }
+
+        
+        Console.WriteLine($"\nTotal players in the party: {players.Count}");
+    }
+}
